5b08cc54739c78eb086ce370d49878d29c4a576b
VeeraNagaDurga <garlankadurgal@gmail.com>
2026-05-24 15:32:01 +0530

    Updated frontend API for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ecommerce-app-9bg2.onrender.com',
         changeOrigin: true,
         secure: false,
       },
